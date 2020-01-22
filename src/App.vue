6736e83603bb399edd40373e846b3c1de69ff60d
Peter Shapkin <rainmaker-90@mail.ru>
2020-01-23 00:10:52 +0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -9,10 +9,11 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+
   name: 'app',
-  components: {
-    HelloWorld
-  }
+
+  components: { HelloWorld }
+
 }
 </script>
 
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const DB_URL = 'https://esp32db-d4ce0.firebaseio.com/'
