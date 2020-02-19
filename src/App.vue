b62b361246da3d807a5f7552ee974fbc79bb280e
Peter Shapkin <rainmaker-90@mail.ru>
2020-02-19 22:17:49 +0500

    Введены стили, merge данных, обработчик once

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,14 @@ export default {
 </script>
 
 <style lang="stylus">
-#app {
-  font-family Arial sans-serif
+body
+  margin 0
+
+#app
+  font-family sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-  margin-top 60px
-}
+
 </style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,32 +1,48 @@
 <template>
   <div>
-    <table
-          v-for="(view, key) in addresses"
-          :key="view.mac + Date.now() + key"
+    <div
+       v-for="(item, key) in addresses"
+       :key="item.id "
+       class="container"
     >
-      <thead>
-        <tr @click="addresses[key][key] = !addresses[key][key]">
-          <td>{{view.mac}}</td>
-          <td>{{formatDate(view.time)}}</td>
-        </tr>
-      </thead>
+      <div
+          class="timeMac-wrapper"
+          @click="addresses[key][key] = !addresses[key][key]"
+      >
+        <span
+           v-text="item.mac"
+           class="mac"
+           :class="[addresses[key][key] ? '__picked' : '', 'mac']"
+        />
+        <span
+           :class="[addresses[key][key] ? '__picked' : '', 'time']"
+           v-text="formatDate(item.time)"
+        />
+      </div>
 
-      <tbody v-if="addresses[key][key]">
-        <tr
-           v-for="state in view.state"
+      <ul
+         class="states"
+         v-if="addresses[key][key]"
+      >
+        <li
+           class="shelf"
+           v-for="state in item.state"
            :key="state[0]"
         >
-          <td>{{state[0]}}</td>
-          <td
-                  v-for="ioState in state[1]"
-                  :key="Date.now() + ioState + Math.random()"
-          >
-            {{ioState}}
-          </td>
-        </tr>
-      </tbody>
-
-    </table>
+          <span
+             class="shelfNumber"
+             v-text="state[0]"
+          />
+          <ul class="IOstates">
+            <li
+               v-for="IO in state[1]"
+               :key="IO.id"
+               :class="[IO === 1 ? '__green' : '__red', 'IO']"
+            />
+          </ul>
+        </li>
+      </ul>
+    </div>
 
     <input
             type="file"
@@ -45,6 +61,7 @@
 <script>
   import firebase from 'firebase/app'
 
+
   export default {
 
   name: 'Dashboard',
@@ -59,35 +76,42 @@
   created() {
     firebase.database()
             .ref('/data')
-            .orderByKey()
-            .limitToLast(20)
-            .on(
-                    'value',
-                    response => {
+            .limitToLast(3) // опционально
+            .once('value')
+            .then(
+                snapshot => {
+                    this.addresses = snapshot.val()
 
-                      this.addresses = {}
-
-                      const databaseObject = Object.fromEntries(
-                              Object.entries(response.val()).reverse()
-                      )
-
-                      for (const key in databaseObject) {
+                    for (const key in this.addresses) {
 
                         this.$set(
-                                this.addresses,
-                                [key],
-                                databaseObject[key]
+                            this.addresses[key],
+                            [key],
+                            false
                         )
-
-                        this.$set(
-                                this.addresses[key],
-                                [key],
-                                false
-                        )
-                      }
 
                     }
+                }
             )
+
+    firebase.database()
+        .ref('/data')
+        .on(
+            'child_added',
+            child => {
+                this.$set(
+                    this.addresses,
+                    [child.key],
+                    child.val()
+                )
+
+                this.$set(
+                    this.addresses[child.key],
+                    [child.key],
+                    false
+                )
+            }
+        )
   },
 
   methods: {
@@ -141,15 +165,74 @@
 </script>
 
 <style scoped lang="stylus">
-.view
-  font 700 32px Arial sans-serif
 
-table
-  border 1px solid red
+.container
+    display flex
+    background-color black
+
+.timeMac-wrapper
+    display flex
+    flex 0 0 25%
+    flex-direction column
+    font-size 48px
+    cursor pointer
+
+.mac
+    color saddlebrown
+    font-weight bold
+    &.__picked
+        color gold
+        text-shadow 0 0 20px yellow, 0 0 10px yellow, 0 0 5px yellow
+.time
+    color maroon
+    font-size 28px
+    &.__picked
+        animation neon 5s ease-in-out alternate infinite
 
 
-td
-  border 1px solid black
+.states
+    flex 0 1 75%
+
+.shelf
+    display flex
+    margin-bottom 15px
+
+
+.shelfNumber
+    font-size 28px
+    font-weight bold
+    flex 0 0 5%
+    color gold
+
+
+.IOstates
+    display flex
+    align-items center
+    justify-content space-between
+    width 100%
+
+.IO
+    width 25px
+    height @width
+    border-radius 50%
+
+.__green
+    background-color lime
+    box-shadow 0 0 15px 7px limegreen
+
+.__red
+    background-color red
+    box-shadow 0 0 15px 7px firebrick
+
+
+ul
+    list-style none
+    padding 0
+    margin 0
+
+
+div
+    border 1px solid black
 
 
 button
@@ -169,41 +252,36 @@ button:active
   box-shadow 0 0
 
 
-.TD
-  margin 0
+@keyframes neon
+    0%
+        color maroon
+        text-shadow 0 0 7px maroon
 
-.TDRR
-  margin 0
+    1%
+        color red
+        text-shadow 0 0 5px red
 
+    20%
+        text-shadow 0 0 5px maroon
+
+    25%
+        color red
+        text-shadow 0 0 7px red
+
+    35%
+        color maroon
+        text-shadow 0 0 7px maroon
+
+    36%
+        color red
+        text-shadow 0 0 5px red
+
+    37%
+        color maroon
+        text-shadow 0 0 5px maroon
+
+    38%
+        color red
+        text-shadow 0 0 5px red
 </style>
 
-
-
-<!--<table-->
-<!--        v-for="(view, key) in views"-->
-<!--        :key="key"-->
-<!--&gt;-->
-<!--  <thead>-->
-<!--  <tr @click="addresses[key] = showAddress === key ? null : key">-->
-<!--    <td>{{view.mac}}</td>-->
-<!--    <td>{{formatDate(view.time)}}</td>-->
-<!--  </tr>-->
-<!--  </thead>-->
-
-<!--  <tbody v-if="showAddress === key ">-->
-<!--  <tr-->
-<!--          v-for="state in view.state"-->
-<!--          :key="state[0]"-->
-<!--  >-->
-<!--    <td class="TD">{{state[0]}}</td>-->
-<!--    <td-->
-<!--            v-for="(ioState, key) in state[1]"-->
-<!--            :key="key + Date.now() + view.mac"-->
-<!--            class="TDRR"-->
-<!--    >-->
-<!--      {{ioState}}-->
-<!--    </td>-->
-<!--  </tr>-->
-<!--  </tbody>-->
-
-<!--</table>-->
